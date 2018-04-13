41086f189b3fcb5ae840d9223bdf522ee4c87b0a
pinkopaio8 <emanuele.siena98@gmail.com>
2018-04-13 16:10:20 +0200

    Seconda Versione

--- calculator.h
+++ calculator.h
@@ -1,33 +1,52 @@
 #pragma once
 
-class Operator {
+class Coniche {
 protected:
+	int a, b, c;
+
+	virtual double asseSimm() = 0;
+	virtual double 
+	virtual double 
+	virtual double 
+	virtual double 
+	virtual double 
+	virtual double 
+	virtual double 
+};
+
+
+class Parabole : public Coniche
+{
+public:
 	virtual double addizione() = 0;
 	virtual double sottrazione() = 0;
 	virtual double moltiplicazione()  = 0;
-	virtual double divisione() = 0; 
+	virtual double divisione() = 0;
 };
 
-
-class OperatorPlus : public Operator
-{
-public:
-	virtual double logaritmo() {};
-	virtual double seno() {};
-	virtual double coseno() {};
-	virtual double tan() {};
-	virtual double radice() {};
-	virtual double logNat() {};
-	virtual double potenza() {};
-	virtual double valAss() {};
-};
-
-class Calculator : public OperatorPlus {
+class Iperbole : public Coniche {
 
 public:
 
 
 
+
 };
+
+class Circonferenza : public Coniche {
+
+
+
+};
+
+class Elisse : public Coniche {
+
+public:
+	static double equazCartesiana() {};
+
+
+
+};
+
 
 
