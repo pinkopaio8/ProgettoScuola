4cc1cf6886b6fbc9b53a3fbc3db63d6114d9d6f0
pozzimarco <35688030+pozzimarco@users.noreply.github.com>
2018-04-30 17:23:05 +0200

    eccoli qua

--- calculator.cpp
+++ calculator.cpp
@@ -1,28 +1,27 @@
 #include "calculator.h"
 #include <math.h>
 
-double Parabole::direttrice(int a, int b, int c)
+/*double Parabole::direttrice(int a, int b, int c)
 {
 	return -(1 + (b ^ 2) - (4 * a * c)) / (4 * a);
 }
 
 
-double Parabole::Coniche::asseSimm()
+/*double Parabole::Coniche::asseSimm()
+{
+
+}
+
+/double Parabole::Coniche::equazioneCartX(int a, int b, int c, int x)
 {
 	
-}
 
-double Parabole::Coniche::equazioneCartX(int a, int b, int c, int x)
-{
-	return y = (a * (x ^ 2)) + (b * x) + c;
-}
-
-double Parabole::Coniche::equazioneCartY(int a, int b, int c, int y)
+/*double Parabole::Coniche::equazioneCartY(int a, int b, int c, int y)
 {
 	return x = (a * (y ^ 2)) + (b * y) + c;
 }
 
-double Parabole::Coniche::interAssX(int a, int b, int c, int x)
+/*double Parabole::Coniche::interAssX(int a, int b, int c, int x)
 {
 	y = (a * (x ^ 2)) + (b * x) + c;
 	x = 0;
@@ -34,17 +33,17 @@ double Parabole::Coniche::interAssY(int a, int b, int c, int y)
 	x = (a * (y ^ 2)) + (b * y) + c;
 	y = 0;
 	return y;
-}
+}*/
 
-double Iperbole::equazioneCart(int a, int b, int c, int x, int y)
+/*double Iperbole::equazioneCart(int a, int b, int c, int x, int y)
 {
 	return (x ^ 2 / a ^ 2) - (y ^ 2 / b ^ 2) - 1;
 }
 
 
-double Iperbole::Coniche::interAssX(int a, int b, int c, int x)
+/*double Iperbole::Coniche::interAssX(int a, int b, int c, int x)
 {
-	
+
 	x = a;
 	y = 0;
 	return x;
@@ -55,9 +54,9 @@ double Iperbole::Coniche::interAssY(int a, int b, int c, int y)
 	y = b;
 	x = 0;
 	return y;
-}
+}*/
 
-void Parabole::Coniche::vertice(int a, int b, int c)
+/*void Parabole::Coniche::vertice(int a, int b, int c)
 {
 	double gino, gianfrogno; //nomi dati da pozzi
 	gino = (-b) / (2 * a);
@@ -72,26 +71,27 @@ double Parabole::Coniche::asse()
 double Parabole::Coniche::direttrice()
 {
 
-}
+}*/
 
 
-double Circonferenza::raggio(int a, int b, int c)
+/*double Circonferenza::raggio(int a, int b, int c)
 {
 	if ((a ^ 2) + (b ^ 2) - c > 0)
 	{
 		return sqrt((a ^ 2) + (b ^ 2) - c);
 	}
+	return 2;
 }
 
-double Circonferenza::equazioneCart()
+/*double Circonferenza::equazioneCart()
 {
 	double *r;
 	r = *raggio;
 	return 2 * r * Pi;
 	//riguardare tutto
-}
+}*/
 
-void Elisse::Coniche::vertice(int a, int b, int c)
+/*void Elisse::Coniche::vertice(int a, int b, int c)
 {
 	int A, A1, B, B1;
 
@@ -100,9 +100,9 @@ void Elisse::Coniche::vertice(int a, int b, int c)
 	B = b;
 	B1 = -b;
 
-}
+}*/
 
-double Elisse::asseMaggiore(int a)
+/*double Elisse::asseMaggiore(int a)
 {
 
 	return 2 * a;
@@ -114,7 +114,7 @@ double Elisse::asseMinore(int b)
 	return 2 * b;
 }
 
-void Iperbole::Coniche::vertice(int a, int b, int c)
+/*void Iperbole::Coniche::vertice(int a, int b, int c)
 {
 	int A, A1, B, B1;
 
@@ -123,9 +123,9 @@ void Iperbole::Coniche::vertice(int a, int b, int c)
 	B = b;
 	B1 = -b;
 
-}
+}*/
 
-void Iperbole::asintoti(int a, int b, int c, int x, int y)
+/*void Iperbole::asintoti(int a, int b, int c, int x, int y)
 {
 	y = -(b / a) * x;
 	y = b / a * x;
@@ -138,6 +138,8 @@ double Elisse::equazioneCart(int a, int b, int c, int x, int y)
 		return (x ^ 2 / a ^ 2) - (y ^ 2 / b ^ 2) - 1;
 	}
 	else
-	{ }
-}
+	{
+		return 0;
+	}
+}*/
 
--- calculator.h
+++ calculator.h
@@ -1,23 +1,24 @@
 #pragma once
+#include <math.h>
+using namespace std;
 
-enum Scelta {
-	Iperbole = 1, Parabola, Elisse, Circonferenza; };
+
 
 class Coniche {
 public:
 	int a, b, c, x, y;
 
-	virtual double asseSimm() = 0;
+	//virtual double asseSimm() = 0; -----> ma perchè
 	virtual double equazioneCartX(int a, int b, int c, int x) = 0;
 	virtual double equazioneCartY(int a, int b, int c, int y) = 0;
-	virtual double interAssX(int a, int b, int c, int x)=0;
-	virtual double interAssY(int a, int b, int c, int y)=0;
+	virtual double interAssX(int a, int b, int c, int x) = 0;
+	virtual double interAssY(int a, int b, int c, int y) = 0;
 	virtual void vertice(int a, int b, int c);
-	virtual double asse();
-	virtual double direttrice();
+	//virtual double asse();  -----> non la utilizziamo
+	//virtual double direttrice(); -----> mi sta sul cazzo
 
 
-	Coniche() {};
+	//Coniche() {}; ma seriamente
 
 
 };
@@ -26,17 +27,75 @@ public:
 class Parabole : public Coniche
 {
 public:
-	virtual double direttrice(int a, int b, int c);
+	double asseSimm() {};
+	double equazioneCartX(int a, int b, int c, int x) {
+		return y = (a * (x ^ 2)) + (b * x) + c;
+	};
+	double equazioneCartY(int a, int b, int c, int y)
+	{
+		return x = (a * (y ^ 2)) + (b * y) + c;
+	};
+	double interAssX(int a, int b, int c, int x)
+	{
+		y = (a * (x ^ 2)) + (b * x) + c;
+		x = 0;
+		return x;
+	};
+	double interAssY(int a, int b, int c, int y)
+	{
+		x = (a * (y ^ 2)) + (b * y) + c;
+		y = 0;
+		return y;
+	};
+	// manca la funzione vertice
+
+
+	virtual double direttrice(int a, int b, int c)
+	{
+		return -(1 + (b ^ 2) - (4 * a * c)) / (4 * a);
+	};
 
 	Parabole() {};
-	
+
 };
 
 class Iperbole : public Coniche {
 
 public:
-	virtual void asintoti(int a, int b, int c, int x, int y);
-	virtual double equazioneCart(int a, int b, int c, int x, int y);
+	// manca equazione cartesiana X e Y
+	double interAssX(int a, int b, int c, int x)
+	{
+
+		x = a;
+		y = 0;
+		return x;
+	};
+	double interAssY(int a, int b, int c, int y)
+	{
+		y = b;
+		x = 0;
+		return y;
+	};
+	void vertice(int a, int b, int c)
+	{
+		int A, A1, B, B1;
+
+		A = a;
+		A1 = -a;
+		B = b;
+		B1 = -b;
+
+	};
+
+
+	virtual void asintoti(int a, int b, int c, int x, int y) {
+		y = -(b / a) * x;
+		y = b / a * x;
+	};
+	virtual double equazioneCart(int a, int b, int c, int x, int y)
+	{
+		return (x ^ 2 / a ^ 2) - (y ^ 2 / b ^ 2) - 1;
+	};
 
 
 
@@ -45,28 +104,60 @@ public:
 class Elisse : public Coniche {
 
 public:
-	virtual double asseMaggiore(int a);
-	virtual double asseMinore(int b);
-	virtual double equazioneCart(int a, int b, int c, int x, int y);
+	double asseSimm() {};
+	double asse() {};
+	void vertice(int a, int b, int c) {
+		int A, A1, B, B1;
+
+		A = a;
+		A1 = -a;
+		B = b;
+		B1 = -b;
+	};
+	virtual double asseMaggiore(int a) {
+
+		return 2 * a;
+	};
+	virtual double asseMinore(int b) {
+
+		return 2 * b;
+	};
+	virtual double equazioneCart(int a, int b, int c, int x, int y) {
+		if (a > b)
+		{
+			return (x ^ 2 / a ^ 2) - (y ^ 2 / b ^ 2) - 1;
+		}
+		else
+		{
+			return 0;
+		}
+	};
 
 
 
 };
 
-class Circonferenza{
+class Circonferenza {
 
 public:
 #define Pi 3,14159;
-	virtual double centro();
-	virtual double realta();
-	virtual double raggio(int a, int b, int c);
-	virtual double equazioneCart();
-	virtual double Area();
-	virtual double circonferenza();
+	virtual double centro() {};
+	virtual double realta() {};
+	virtual double raggio(int a, int b, int c) {
+		if ((a ^ 2) + (b ^ 2) - c > 0)
+		{
+			return sqrt((a ^ 2) + (b ^ 2) - c);
+		}
+		return 2;
+	};
+	virtual double equazioneCart() {};
+	virtual double Area() {};
+	virtual double circonferenza() {};
 
 
 };
 
+//istream& cin >>
 
 
 
